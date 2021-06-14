715675e0f0772be6d497b79f3d7e9306d507bd3e
Mieli <alessandro.mieli@gmail.com>
2021-06-13 22:55:42 -0300

    Adicionado chamada ao proxy para autenticaçao do Login

--- src/service/AuthorizatonService.js
+++ src/service/AuthorizatonService.js
@@ -1,4 +1,11 @@
-
+/**
+ * A função authenticated é responsável por fazer a chamado ao serivor passando as credenciais de acesso  
+ * 
+ * 
+ * @author Mieli
+ * @param credential (credenciais de acesso)
+ * @returns TRUE / FALSE 
+ */
 export async function authenticated(credential) {
     let result = false;
     let response = null;
@@ -22,23 +29,50 @@ export async function authenticated(credential) {
     return result;
 }
 
+
+/**
+ * A função getCurrentUser obtem o usuário que realizou ou login, os dados do usuário esta no localStorage  
+ * 
+ * 
+ * @author Mieli
+ * @param 
+ * @returns usuário do localStorage 
+ */
 export function getCurrentUser(){
     const localStorage = window.localStorage;
     const currentUser = localStorage.getItem("CURRENT_USER");
     return convertStringToObject(currentUser);
 }
 
+
+/**
+ * A função convertObjectToString converte um objeto em string para armazenar no localStorage 
+ * 
+ * 
+ * @author Mieli
+ * @param object
+ * @returns uma string  / ""
+ */
 function convertObjectToString(object){
     if(object){
         return JSON.stringify(object);
     }
-    return null;
+    return "";
 }
 
+
+/**
+ * A função convertStringToObject converte uma string em objeto 
+ * 
+ * 
+ * @author Mieli
+ * @param string
+ * @returns um objeto  / null
+ */
 function convertStringToObject(string){
     if(string){
         return JSON.parse(string);
     }
-    return "";
+    return null;
 }
 
